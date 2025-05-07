3640d505e7f0dd2a3e6b2243d4036ce8feb61518
Boniface <bm715452@gmail.com>
2025-05-07 09:57:53 +0300

    instance methods tests

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
+    def __init__(self, name="Unnamed", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
 
     #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
     pass
